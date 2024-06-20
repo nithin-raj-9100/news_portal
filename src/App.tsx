1ec333527d29d95fe2df23d91be1ef44dc928b24
Nithin Raj <nithinraj9100@gmail.com>
2024-06-20 10:24:19 +0530

    checking deplyoment

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,27 @@ import { Button } from "./components/ui/button";
 function App() {
   const [news, setNews] = useState([]);
   const [error, setError] = useState(null);
+  const url = "https://newsapi.org/v2/everything?";
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${import.meta.env.VITE_API_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setNews(data.articles);
+      });
+  }, []);
 
   return (
     <>
       <Button>Click me</Button>
+      {news.map((item) => (
+        <div key={item.title}>
+          <h1>{item.title}</h1>
+          <p>{item.description}</p>
+        </div>
+      ))}
     </>
   );
 }
